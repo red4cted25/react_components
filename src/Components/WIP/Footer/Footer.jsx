152f1732d875eb25c74d9324ec653dae27329152
Nico <141285419+red4cted25@users.noreply.github.com>
2024-11-22 14:35:50 -0700

    Section Footer
    Section footer into code-able sections through the jsx file for later filling and styling

--- src/Components/WIP/Footer/Footer.jsx
+++ src/Components/WIP/Footer/Footer.jsx
@@ -4,9 +4,44 @@ import { FaGithub, FaInstagram, FaXTwitter } from "react-icons/fa6";
 function footerClassic() {
     return (
         <footer className="footer-classic">
-            <div className="footer-content">
-                <p>Built with React and CSS</p>
-            </div>
+            <section className="footer-content">
+                <div className="footer-logo-info">
+                    <img src="logo.png" alt="Logo" />
+                    <p className="footer-info">123 Sesame Street</p>
+                    <p className="footer-info">(212) 875-6890</p>
+                    <p className="footer-info">Web.ShowKids@sesame.org</p>
+                </div>
+                <section className="footer-navigation">
+                    <div className="support">
+                        <h3>Support</h3>
+                        <ul>
+                            <li>Contact</li>
+                            <li>FAQs</li>
+                            <li>Pricing Plans</li>
+                            <li>Sitemap</li>
+                        </ul>
+                    </div>
+                    <div className="quick-links">
+                        <h3>Quick Links</h3>
+                        <ul>
+                            <li>Jobs</li>
+                            <li>Courses</li>
+                            <li>Paid Training</li>
+                            <li>Blog</li>
+                        </ul>
+                    </div>
+                    <div className="category">
+                        <h3>Category</h3>
+                        <ul>
+                            <li>Graphics</li>
+                            <li>Programming</li>
+                            <li>eCommerce</li>
+                            <li>Freelancing</li>
+                        </ul>
+                    </div>
+                </section>
+            </section>
+            <div className="up-button"></div>
             <div className="footer-end">
                 <p>Copyright © 2024 [Company Name] Developed by <a href="https://github.com/red4cted25" id='footer-dev-link'>Nico Diaz</a></p>
                 <div className="social-media-icons">
